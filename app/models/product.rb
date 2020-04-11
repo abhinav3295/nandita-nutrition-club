4e32af328bdf7687c4c49325140dc195b0ca98b4
Abhinav Singh Suryavanshi <abhinav3295@gmail.com>
2020-04-11 12:14:42 +0530

    remove pry from prod code

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,9 @@
-require 'pry'
 class Product < ApplicationRecord
   has_many :stocks
   validates_presence_of :name
   validates :name, length: { minimum: 5, maximum: 80 }
 
   def before_destroy
-    binding.pry
     stocks.each do |s|
       s.destroy
     end
